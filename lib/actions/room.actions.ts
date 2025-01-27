661f23ba6caea466d46922bb882a7eb5246eec23
andresmarchenaporras@gmail.com <Andres37DM>
2025-01-27 16:18:38 -0500

    Adjustments of title and the home page

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,86 @@
+"use server";
+
+import { nanoid } from "nanoid";
+import { liveblocks } from "../liveblocks";
+import { revalidatePath } from "next/cache";
+import { parseStringify } from "../utils";
+
+export const createDocument = async ({
+  userId,
+  email,
+}: CreateDocumentParams) => {
+  const roomId = nanoid();
+
+  try {
+    const metadata = {
+      creatorID: userId,
+      email,
+      title: "Documento sin título",
+    };
+
+    const usersAccesses: RoomAccesses = {
+      [email]: ["room:write"],
+    };
+
+    const room = await liveblocks.createRoom(roomId, {
+      metadata,
+      usersAccesses,
+      defaultAccesses: ["room:write"],
+    });
+
+    revalidatePath("/");
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getDocument = async ({
+  roomId,
+  userId,
+}: {
+  roomId: string;
+  userId: string;
+}) => {
+  try {
+    const room = await liveblocks.getRoom(roomId);
+
+    // TODO: Bring this back when we have the permissions system
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+
+    // if (!hasAccess) {
+    //   throw new Error("No tienes acceso a este documento");
+    // }
+
+    return parseStringify(room);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateDocument = async (roomId: string, title: string) => {
+  try {
+    const updateRoom = await liveblocks.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+
+    revalidatePath(`document/${roomId}`);
+
+    return parseStringify(updateRoom);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getDocuments = async (email: string) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(error);
+  }
+};
